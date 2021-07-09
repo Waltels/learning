c447fa1857a18437f588e2af4c589800d7496329
Walter Laura Soto <waltels20219@gmail.com>
2021-07-09 17:48:47 -0400

    proyecto inicial hasta laravel lang

---
+++ app/Models/Unit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Unit extends Model
+{
+    const ZIP = 'ZIP';
+    const VIDEO = 'VIDEO';
+    const SECTION = 'SECTION';
+}
